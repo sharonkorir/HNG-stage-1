160dd3c7c8fda6c9c92cedf254adb2bab50f0713
sharonkorir <sharon.korir@student.moringaschool.com>
2022-11-01 12:02:25 +0300

    Styling: add card panels and center the link section

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@
 //   );
 // }
 
-import M from 'materialize-css';
+// import M from 'materialize-css';
 
 function App() {
   return(
@@ -33,18 +33,49 @@ function App() {
         <p>sharon korir id, slack, className hidden</p>
         <p>share button /flex..more button on small screen</p>
       </header>
-      <div className="link-section">
-        <a href="https://twitter.com/coding_architct">Twitter Link</a>
-        <a href="https://training.zuri.team/">Zuri Team id btn_zuri</a>
-        <a href="https://books.zuri.team">Zuri Books id 'books'</a>
-        <a href="https://books.zuri.team/python-for-beginners?ref_id=sharon korir">Python Books id 'book_python'</a>
-        <a href="https://background.zuri.team">Background Check for Coders 'pitch'</a>
-        <a href="https://books.zuri.team/design-rules">Design Books id 'book_design'</a>
+      <div className="row">
+        <div className="col s12 m12 l12">
+          <div className="card-panel grey lighten-3 center-align">
+            <a href="https://twitter.com/coding_architct" className='black-text'>Twitter Link</a>
+          </div>
+        </div>
+        <div className="col s12 m12 l12">
+          <div className="card-panel grey lighten-3 center-align">
+            <a href="https://training.zuri.team/" className='black-text'>Zuri Team id btn_zuri</a>
+          </div>
+        </div>
+        <div className="col s12 m12 l12">
+          <div className="card-panel grey lighten-3 center-align">
+            <a href="https://books.zuri.team" className='black-text'>Zuri Books id 'books'</a>
+          </div>
+        </div>
+        <div className="col s12 m12 l12">
+          <div className="card-panel grey lighten-3 center-align">
+            <a href="https://books.zuri.team/python-for-beginners?ref_id=sharon korir" className='black-text'>Python Books id 'book_python'</a>
+          </div>
+        </div>
+        <div className="col s12 m12 l12">
+          <div className="card-panel grey lighten-3 center-align">
+            <a href="https://background.zuri.team" className=' black-text'>Background Check for Coders 'pitch'</a>
+          </div>
+        </div>
+        <div className="col s12 m12 l12">
+          <div className="card-panel grey lighten-3 center-align">
+            <a href="https://books.zuri.team/design-rules" className='black-text'>Design Books id 'book_design'</a>
+          </div>
+        </div>
+        {/* <a href="https://twitter.com/coding_architct" className='col s12 m12 l12 card-panel grey lighten-2 black-text center-align'>Twitter Link</a>
+        <a href="https://training.zuri.team/" className='col s12 m12 l12 card-panel grey lighten-2 black-text center-align'>Zuri Team id btn_zuri</a>
+        <a href="https://books.zuri.team" className='col s12 m12 l12 card-panel grey lighten-2 black-text center-align'>Zuri Books id 'books'</a>
+        <a href="https://books.zuri.team/python-for-beginners?ref_id=sharon korir" className='col s12 m12 l12 card-panel grey lighten-2 black-text center-align'>Python Books id 'book_python'</a>
+        <a href="https://background.zuri.team" className='col s12 m12 l12 card-panel grey lighten-2 black-text center-align'>Background Check for Coders 'pitch'</a>
+        <a href="https://books.zuri.team/design-rules" className='col s12 m12 l12 card-panel grey lighten-2 black-text center-align'>Design Books id 'book_design'</a> */}
       </div>
       <div className="icons">
         <p>slack icon</p>
         <a href="https://github.com/sharonkorir">github icon</a>
       </div>
+      <div className="divider"></div>
       <div className="footer">
         <p>zuri internship icon...flex on wide screen, cascade on mobile</p>
         <p>HNG Internship 9 Frontend Task</p>
